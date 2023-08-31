669a9424ea3d0e1e73ef99d61832dc38c8febee8
Scotty Jirik <ScottyJirik@MacBook-Pro.local>
2023-08-31 15:43:25 -0500

    Fixed up my JS some more, cannot get access to the API due to CORS?

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,22 @@
 var userInput = document.querySelector('#form');
-var languageButtonsEl = document.querySelector('#language-buttons');
 var cityInput = document.querySelector('#city');
 var dataContainer = document.querySelector('#data-container');
 var dataSearched = document.querySelector('#city-search');
 
+var selectedCity = cityInput.value.trim();
+
 var userSubmit = function (event) {
   event.preventDefault();
 
-  var city = cityInput.value.trim();
-
-  if (city) {
-    getWeather(city);
+  if (selectedCity) {
+    getWeather(selectedCity);
     dataContainer.textContent = '';
-    cityInput.value = '';
+    cityInput.value = ''; 
   } else {
     alert('Please enter a City');
   }
 };
+
 
 var getWeather = function (place) {
   var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=02a7b6b8e73b5f959b18d22551b1f45b';
@@ -37,5 +37,32 @@ var getWeather = function (place) {
 };
 
 
+var displayWeather = function (cities, searchTerm) {
+  if (cities.length === 0) {
+    dataContainer.textContent = 'Please enter a real City';
+    return;
+  }
+
+  dataSearched.textContent = searchTerm;
+
+  for (var i = 0; i < cities.length; i++) {
+    var cityName = cities[i].city.name;
+
+    var cityEl = document.createElement('div');
+    cityEl.classList = 'list-item flex-row justify-space-between align-center';
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = cityName;
+
+    cityEl.appendChild(titleEl);
+
+    var statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
+
+    cityEl.appendChild(statusEl);
+
+    dataContainer.appendChild(cityEl);
+  }
+};
 
 userInput.addEventListener('submit', userSubmit);
